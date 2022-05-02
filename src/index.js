07234f145c31c6483fe9c32cfd4b50889927e3d9
Irina <mandarinka.21.09.00@gmail.com>
2022-05-02 12:11:12 +0300

    poke
    fetch to pokeapi and display first 10 pokemons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import getPokes from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('poke'));
+let pokeArr = getPokes();
+pokeArr.then((response) => {
+    let result = response.map(poke => <div>{poke.name}</div>);
+    root.render(result);
+})
+    .catch(reject => root.render(<div>Something going wrong :(</div>));
