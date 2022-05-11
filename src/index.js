acd060ffe531a266bdfbc288c683568a73db7828
Irina <mandarinka.21.09.00@gmail.com>
2022-05-11 13:36:15 +0300

    list
    added function list and pokemon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {useState} from "react";
 
 
 const root = ReactDOM.createRoot(document.getElementById('poke'));
 let pokeArr = getPokes();
 pokeArr.then((response) => {
-    let result = response.map(poke => <div>{poke.name}</div>);
-    root.render(result);
+        root.render(<List array={response}/>);
 })
     .catch(reject => root.render(<div>Something going wrong :(</div>));
 
 
+function List(props){
+    return props.array.map(item => <Pokemon pokemon={item}/>);
+}
+
+function Pokemon(props){
+    return <div>{props.pokemon.name}</div>;
+}
 
 async function getPokes(offset = 1, limit = 10) {
     let url = "https://pokeapi.co/api/v2/pokemon/";
