8fa2fceecf115321bc5dd2ab457860117d3c7e91
Irina <mandarinka.21.09.00@gmail.com>
2022-05-16 14:18:35 +0300

    usingHooks
    use state, effect and callback hooks

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {useState} from "react";
+import {useState, useEffect, useCallback} from "react";
 
 
 const root = ReactDOM.createRoot(document.getElementById('poke'));
-let pokeArr = getPokes();
-pokeArr.then((response) => {
-        root.render(<List array={response}/>);
-})
-    .catch(reject => root.render(<div>Something going wrong :(</div>));
+root.render(<Pokemon/>)
+
+function Pokemon() {
+    const [page, setPage] = useState(0);
+    const [limit, setLimit] = useState(10)
+
+    const getPokesFromApi = useCallback(() => {
+        let offset = page * limit;
+        let url = "https://pokeapi.co/api/v2/pokemon/";
+        return fetch(url + `?offset=${offset}&limit=${limit}`)
+            .then(response => response.json())
+            .then(response => response.results);
+    }, [page, limit]);
 
 
-function List(props){
-    return props.array.map(item => <Pokemon pokemon={item}/>);
+    return (
+        <section>
+            <ListPokemons getPokemons={getPokesFromApi}/>
+            <button onClick={() => setPage(prev => ++prev)}>Next Page</button>
+            <input type="text" onInput={(e) => setLimit(e.target.value)}/>
+        </section>
+    );
 }
 
-function Pokemon(props){
-    return <div>{props.pokemon.name}</div>;
-}
+function ListPokemons({getPokemons}) {
+    const [pokemons, setPokemons] = useState([]);
 
-async function getPokes(offset = 1, limit = 10) {
-    let url = "https://pokeapi.co/api/v2/pokemon/";
-    let response = await fetch(url + `?offset=${offset}&limit=${limit}`)
-        .then(response => response.json())
-        .then(response => response.results);
-    return response;
+    useEffect(() => {
+        getPokemons().then(pokes => setPokemons(pokes));
+    }, [getPokemons]);
+
+    return pokemons.map(item => <div key={item.name}>{item.name}</div>)
 }
